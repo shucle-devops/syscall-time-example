b44aee2365084759314c4c17ec12d4568ffa35e1
강영주 <yeongjukang@hyundai.com>
2021-06-15 18:20:45 +0900

    client observation code

--- client/main.go
+++ client/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/yeongjukang/syscall-time-example/proto"
@@ -22,12 +23,23 @@ func main() {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		fmt.Printf("[CLIENT] %s\n", time.Now().Format("2006-01-02 15:04:05"))
+		time.Sleep(time.Second * 3)
+		fmt.Printf("[CLIENT] %s\n", time.Now().Format("2006-01-02 15:04:05"))
+		wg.Done()
+	}()
+
 	c := proto.NewExampleServiceClient(conn)
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
+
 	r, err := c.PostgresExample(ctx, &proto.Request{Count: 5})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
+	wg.Wait()
 	fmt.Printf("[CLIENT] RESULT : last ptypes - %s, last pg - %s\n", r.LastPtypesTime, r.LastPgTime)
 }
